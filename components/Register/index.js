2c3c7ecd3392fd24de57eb0018bc51b92e8b856c
fatihguzel <20060393@stu.omu.edu.tr>
2022-12-19 17:42:14 +0300

    #7 Profile Sayfası Oluşturuldu

--- components/Register/index.js
+++ components/Register/index.js
@@ -103,7 +103,7 @@ const index = () => {
       </div>
       <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
         <div className="text-white mb-3 mb-md-0">
-          Copyright © 2020. All rights reserved.
+          Copyright © 2022 All rights reserved by BlogFly
         </div>
       </div>
     </section>
