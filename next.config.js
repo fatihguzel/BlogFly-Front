20801c98ac1509c72b4e55a238b07ce2ae7f97a1
FATİH GÜZEL <98105601+fatihguzel@users.noreply.github.com>
2023-01-03 17:53:00 +0300

    change server url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     API_URL:
       process.env.NODE_ENV === "production"
-        ? "https://blogfly.herokuapp.com"
+        ? "https://blogflyserver.herokuapp.com"
         : "http://localhost:5000",
   },
 };
